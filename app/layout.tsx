f68348d754195e7fc751408da784d9dcfef2d2be
Gabriel Souza <gabrielsouzarodrigues.dev@gmail.com>
2025-06-21 10:32:35 -0300

    +

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'G10 Importações',
+  description: 'Desenvolvido por Gabriel Rodrigues',
+  generator: 'Dev Gabriel',
 }
 
 export default function RootLayout({
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="PT-BR">
       <body>{children}</body>
     </html>
   )
